297828499d29f68384017ac11b03aa6c0a162705
Chris Karr <github@audacious-software.com>
2012-10-02 05:33:54 -0500

    Javascript fixes for probe triggers.

---
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/plugins/LogCatOutputPlugin.java
@@ -0,0 +1,40 @@
+package edu.northwestern.cbits.purple_robot_manager.plugins;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import edu.northwestern.cbits.purple_robot_manager.probes.Probe;
+
+public class LogCatOutputPlugin extends OutputPlugin
+{
+	public String[] respondsTo()
+	{
+		String[] activeActions = { OutputPlugin.OUTPUT_EVENT, Probe.PROBE_READING };
+
+		return activeActions;
+	}
+
+	public void processIntent(Intent intent)
+	{
+		JSONObject object = new JSONObject();
+
+		try
+		{
+			object.put("intent_action", intent.getAction());
+
+			Bundle extras = intent.getExtras();
+
+			object.put("extras", OutputPlugin.jsonForBundle(extras));
+
+			Log.e("PRM", "JSON OBJECT: " + object.toString());
+		}
+		catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/plugins/TriggerOutputPlugin.java
@@ -0,0 +1,49 @@
+package edu.northwestern.cbits.purple_robot_manager.plugins;
+
+import java.util.List;
+
+import org.json.JSONException;
+
+import android.content.Context;
+import android.content.Intent;
+import edu.northwestern.cbits.purple_robot_manager.JSONConfigFile;
+import edu.northwestern.cbits.purple_robot_manager.probes.Probe;
+import edu.northwestern.cbits.purple_robot_manager.triggers.ProbeTrigger;
+import edu.northwestern.cbits.purple_robot_manager.triggers.Trigger;
+
+public class TriggerOutputPlugin extends OutputPlugin
+{
+	public String[] respondsTo()
+	{
+		String[] activeActions = { Probe.PROBE_READING };
+
+		return activeActions;
+	}
+
+	public void processIntent(Intent intent)
+	{
+		Context context = this.getContext();
+
+		try
+		{
+			JSONConfigFile jsonConfig = new JSONConfigFile(context);
+
+			List<Trigger> triggers = jsonConfig.getTriggers();
+
+			for (Trigger trigger : triggers)
+			{
+				if (trigger instanceof ProbeTrigger)
+				{
+					ProbeTrigger probeTrigger = (ProbeTrigger) trigger;
+
+					if (probeTrigger.matches(context, OutputPlugin.jsonForBundle(intent.getExtras())))
+						trigger.execute(context);
+				}
+			}
+		}
+		catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
