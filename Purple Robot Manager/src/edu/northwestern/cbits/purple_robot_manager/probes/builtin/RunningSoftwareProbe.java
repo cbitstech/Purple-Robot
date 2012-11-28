4da76a4b0f98d33820ca7b76608f96763cadcf35
Chris Karr <github@audacious-software.com>
2012-11-28 14:06:38 -0600

    Splitting up software probes.

--- Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/builtin/RunningSoftwareProbe.java
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/builtin/RunningSoftwareProbe.java
@@ -1,16 +1,14 @@
 package edu.northwestern.cbits.purple_robot_manager.probes.builtin;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -20,27 +18,22 @@ import android.preference.PreferenceScreen;
 import edu.northwestern.cbits.purple_robot_manager.R;
 import edu.northwestern.cbits.purple_robot_manager.probes.Probe;
 
-public class SoftwareInformationProbe extends Probe
+public class RunningSoftwareProbe extends Probe
 {
-	private static final String CODENAME = "CODENAME";
-	private static final String INCREMENTAL = "INCREMENTAL";
-	private static final String RELEASE = "RELEASE";
-	private static final String SDK_INT = "SDK_INT";
-	private static final String APP_NAME = "APP_NAME";
 	private static final String PACKAGE_NAME = "PACKAGE_NAME";
-	private static final String INSTALLED_APPS = "INSTALLED_APPS";
-	private static final String INSTALLED_APP_COUNT = "INSTALLED_APP_COUNT";
+	private static final String RUNNING_TASKS = "RUNNING_TASKS";
+	private static final String RUNNING_TASK_COUNT = "RUNNING_TASK_COUNT";
 
 	private long _lastCheck = 0;
 
 	public String name(Context context)
 	{
-		return "edu.northwestern.cbits.purple_robot_manager.probes.builtin.SoftwareInformationProbe";
+		return "edu.northwestern.cbits.purple_robot_manager.probes.builtin.RunningSoftwareProbe";
 	}
 
 	public String title(Context context)
 	{
-		return context.getString(R.string.title_software_info_probe);
+		return context.getString(R.string.title_running_software_probe);
 	}
 
 	public String probeCategory(Context context)
@@ -56,11 +49,11 @@ public class SoftwareInformationProbe extends Probe
 		{
 			long now = System.currentTimeMillis();
 
-			if (prefs.getBoolean("config_probe_software_enabled", true))
+			if (prefs.getBoolean("config_probe_running_software_enabled", true))
 			{
 				synchronized(this)
 				{
-					long freq = Long.parseLong(prefs.getString("config_probe_software_frequency", "300000"));
+					long freq = Long.parseLong(prefs.getString("config_probe_running_software_frequency", "300000"));
 
 					if (now - this._lastCheck  > freq)
 					{
@@ -68,29 +61,22 @@ public class SoftwareInformationProbe extends Probe
 						bundle.putString("PROBE", this.name(context));
 						bundle.putLong("TIMESTAMP", System.currentTimeMillis() / 1000);
 
-						bundle.putString(SoftwareInformationProbe.CODENAME, Build.VERSION.CODENAME);
-						bundle.putString(SoftwareInformationProbe.INCREMENTAL, Build.VERSION.INCREMENTAL);
-						bundle.putString(SoftwareInformationProbe.RELEASE, Build.VERSION.RELEASE);
-						bundle.putInt(SoftwareInformationProbe.SDK_INT, Build.VERSION.SDK_INT);
+						ActivityManager am = (ActivityManager) context.getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+						ArrayList<RunningTaskInfo> tasks = new ArrayList<RunningTaskInfo>(am.getRunningTasks(9999));
 
-						PackageManager pm = context.getApplicationContext().getPackageManager();
+						ArrayList<Bundle> running = new ArrayList<Bundle>();
 
-						List<ApplicationInfo> infos = pm.getInstalledApplications(0);
-
-						ArrayList<Bundle> installed = new ArrayList<Bundle>();
-
-						for (ApplicationInfo info : infos)
+						for (RunningTaskInfo info : tasks)
 						{
-							Bundle appBundle = new Bundle();
+							Bundle taskBundle = new Bundle();
 
-							appBundle.putString(SoftwareInformationProbe.APP_NAME, info.loadLabel(pm).toString());
-							appBundle.putString(SoftwareInformationProbe.PACKAGE_NAME, info.packageName);
+							taskBundle.putString(RunningSoftwareProbe.PACKAGE_NAME, info.baseActivity.getPackageName());
 
-							installed.add(appBundle);
+							running.add(taskBundle);
 						}
 
-						bundle.putParcelableArrayList(SoftwareInformationProbe.INSTALLED_APPS, installed);
-						bundle.putInt(SoftwareInformationProbe.INSTALLED_APP_COUNT, installed.size());
+						bundle.putParcelableArrayList(RunningSoftwareProbe.RUNNING_TASKS, running);
+						bundle.putInt(RunningSoftwareProbe.RUNNING_TASK_COUNT, running.size());
 
 						this.transmitData(context, bundle);
 
@@ -107,10 +93,9 @@ public class SoftwareInformationProbe extends Probe
 
 	public String summarizeValue(Context context, Bundle bundle)
 	{
-		String release = bundle.getString(SoftwareInformationProbe.RELEASE);
-		int count = bundle.getInt(SoftwareInformationProbe.INSTALLED_APP_COUNT);
+		int count = bundle.getInt(RunningSoftwareProbe.RUNNING_TASK_COUNT);
 
-		return String.format(context.getResources().getString(R.string.summary_software_info_probe), release, count);
+		return String.format(context.getResources().getString(R.string.summary_running_software_probe), count);
 	}
 /*
 	public Bundle formattedBundle(Context context, Bundle bundle)
@@ -136,17 +121,17 @@ public class SoftwareInformationProbe extends Probe
 
 		PreferenceScreen screen = manager.createPreferenceScreen(activity);
 		screen.setTitle(this.title(activity));
-		screen.setSummary(R.string.summary_software_info_probe_desc);
+		screen.setSummary(R.string.summary_running_software_probe_desc);
 
 		CheckBoxPreference enabled = new CheckBoxPreference(activity);
 		enabled.setTitle(R.string.title_enable_probe);
-		enabled.setKey("config_probe_software_enabled");
+		enabled.setKey("config_probe_running_software_enabled");
 		enabled.setDefaultValue(true);
 
 		screen.addPreference(enabled);
 
 		ListPreference duration = new ListPreference(activity);
-		duration.setKey("config_probe_software_frequency");
+		duration.setKey("config_probe_running_software_frequency");
 		duration.setDefaultValue("300000");
 		duration.setEntryValues(R.array.probe_satellite_frequency_values);
 		duration.setEntries(R.array.probe_satellite_frequency_labels);
