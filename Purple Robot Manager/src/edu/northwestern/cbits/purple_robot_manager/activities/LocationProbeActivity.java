cc513d3c58328a5d218c8dfa852407590a14a4df
Chris Karr <github@audacious-software.com>
2012-12-09 01:31:35 -0600

    Labeling interface work.

--- Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/activities/LocationProbeActivity.java
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/activities/LocationProbeActivity.java
@@ -8,21 +8,29 @@ import java.util.List;
 import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockMapActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 import com.google.android.maps.Projection;
 
 import edu.northwestern.cbits.purple_robot_manager.ProbeViewerActivity;
@@ -31,6 +39,78 @@ import edu.northwestern.cbits.purple_robot_manager.probes.builtin.LocationProbe;
 
 public class LocationProbeActivity extends SherlockMapActivity
 {
+	private String _subtitle = null;
+	private double _selectedTimestamp = 0;
+
+	private class PointOverlay extends ItemizedOverlay<OverlayItem>
+	{
+		private List<Location> _locations = null;
+		private LocationProbeActivity _activity = null;
+
+		public PointOverlay(Drawable drawable)
+		{
+			super(drawable);
+		}
+
+		public PointOverlay(Drawable drawable, List<Location> locations, LocationProbeActivity activity)
+	    {
+			super(drawable);
+
+	    	this._locations = new ArrayList<Location>(locations);
+	    	this._activity = activity;
+
+	    	this.populate();
+	    }
+
+		protected boolean onTap(int index)
+		{
+			Log.e("PRM", "TAPPED: " + index);
+
+			Location l = this._locations.get(index);
+
+			final long timestamp = l.getTime();
+
+			Log.e("PRM", "TIMESTAMP: " + timestamp);
+
+			final Date d = new Date((long) timestamp);
+
+			final SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss, MMM d");
+
+			final PointOverlay me = this;
+
+			this._activity.runOnUiThread(new Runnable()
+			{
+				public void run()
+				{
+					ActionBar actionBar = me._activity.getSupportActionBar();
+
+					actionBar.setSubtitle(String.format(me._activity.getString(R.string.display_date_item_count), sdf.format(d), me._activity._subtitle));
+
+					me._activity._selectedTimestamp = (double) timestamp;
+				}
+			});
+
+			return true;
+		}
+
+		protected OverlayItem createItem(int i)
+		{
+			Location l = this._locations.get(i);
+
+			int latitude = (int) (l.getLatitude() * 1000000);
+			int longitude = (int) (l.getLongitude() * 1000000);
+
+	        GeoPoint origin = new GeoPoint(latitude, longitude);
+
+			return new OverlayItem(origin, "", "");
+		}
+
+		public int size()
+		{
+			return this._locations.size();
+		}
+	}
+
 	private class PathOverlay extends Overlay
 	{
 		List<Location> _locations = null;
@@ -105,6 +185,11 @@ public class LocationProbeActivity extends SherlockMapActivity
 	{
 		super.onResume();
 
+		this.refresh();
+	}
+
+	public void refresh()
+	{
 		MapView map = (MapView) this.findViewById(R.id.mapview);
 
 		List<Overlay> overlays = map.getOverlays();
@@ -159,10 +244,54 @@ public class LocationProbeActivity extends SherlockMapActivity
 
 		controller.zoomToSpan(latSpan, lonSpan);
 
-		DisplayMetrics metrics = new DisplayMetrics();
+		final DisplayMetrics metrics = new DisplayMetrics();
 
 		this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-		overlays.add(new PathOverlay(locations, metrics));
+
+		PathOverlay pathOverlay = new PathOverlay(locations, metrics);
+		overlays.add(pathOverlay);
+
+		Drawable d = new Drawable()
+		{
+			public void draw(Canvas canvas)
+			{
+		    	Paint paint = new Paint();
+
+		        paint.setDither(true);
+		        paint.setColor(Color.DKGRAY);
+		        paint.setStyle(Paint.Style.STROKE);
+		        paint.setStrokeJoin(Paint.Join.ROUND);
+		        paint.setStrokeCap(Paint.Cap.ROUND);
+		        paint.setStrokeWidth(metrics.density * 15);
+
+		        Path path = new Path();
+
+		        path.moveTo(0, 0);
+		        path.lineTo(1.0f, 0.0f);
+		        path.lineTo(1.0f, 1.0f);
+		        path.lineTo(0.0f, 1.0f);
+
+		        canvas.drawPath(path, paint);
+			}
+
+			public int getOpacity()
+			{
+				return PixelFormat.OPAQUE;
+			}
+
+			public void setAlpha(int alpha)
+			{
+
+			}
+
+			public void setColorFilter(ColorFilter cf)
+			{
+
+			}
+		};
+
+		PointOverlay pointOverlay = new PointOverlay(d, locations, this);
+		overlays.add(pointOverlay);
 
 		this.getSupportActionBar().setTitle(R.string.title_location_history);
 
@@ -172,9 +301,11 @@ public class LocationProbeActivity extends SherlockMapActivity
 		String endDate = sdf.format(new Date(maxTime));
 
 		if (!startDate.equals(endDate))
-			this.getSupportActionBar().setSubtitle(startDate + " - " + endDate + " (" + locations.size() + ")");
+			this._subtitle = startDate + " - " + endDate + " (" + locations.size() + ")";
 		else
-			this.getSupportActionBar().setSubtitle(startDate + " (" + locations.size() + ")");
+			this._subtitle = startDate + " (" + locations.size() + ")";
+
+		this.getSupportActionBar().setSubtitle(this._subtitle);
 	}
 
 	protected boolean isRouteDisplayed()
@@ -203,7 +334,27 @@ public class LocationProbeActivity extends SherlockMapActivity
 				this.startActivity(dataIntent);
 
     			break;
-		}
+
+    		case R.id.menu_new_label:
+    			if (this._selectedTimestamp == 0)
+    				Toast.makeText(this, R.string.message_missing_timestamp, Toast.LENGTH_LONG).show();
+    			else
+    			{
+
+        			Intent labelIntent = new Intent(this, LabelActivity.class);
+        			labelIntent.putExtra(LabelActivity.TIMESTAMP, this._selectedTimestamp);
+        			labelIntent.putExtra(LabelActivity.LABEL_CONTEXT, this.getIntent().getStringExtra("probe_name"));
+
+        			this.startActivity(labelIntent);
+    			}
+
+    			break;
+
+    		case R.id.menu_refresh:
+    			this.refresh();
+
+    			break;
+    	}
 
     	return true;
     }
