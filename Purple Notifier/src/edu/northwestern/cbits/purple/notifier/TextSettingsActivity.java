cf6902abc593321281ac5f1d4d65c5b45c6fd47d
Chris Karr <github@audacious-software.com>
2013-01-26 01:29:01 -0600

    Initial widget app commit.

---
+++ Purple Notifier/src/edu/northwestern/cbits/purple/notifier/TextSettingsActivity.java
@@ -0,0 +1,29 @@
+package edu.northwestern.cbits.purple.notifier;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+public class TextSettingsActivity extends SherlockPreferenceActivity 
+{
+	@SuppressWarnings("deprecation")
+	public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        this.addPreferencesFromResource(R.layout.layout_text_settings_activity);
+    }
+	
+	public void onResume()
+	{
+		super.onResume();
+
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, this.getIntent().getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID));
+
+        this.setResult(Activity.RESULT_OK, resultValue);
+	}
+}
