0b850450b97610e22dfba39af5b6398da9a31074
Chris Karr <github@audacious-software.com>
2013-01-28 12:59:17 -0600

    Misc. plumbing.

--- Purple Notifier/src/edu/northwestern/cbits/purple/notifier/MainSettingsActivity.java
+++ Purple Notifier/src/edu/northwestern/cbits/purple/notifier/MainSettingsActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainSettingsActivity extends SherlockPreferenceActivity 
 {
-	@SuppressWarnings("deprecation")
 	public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
