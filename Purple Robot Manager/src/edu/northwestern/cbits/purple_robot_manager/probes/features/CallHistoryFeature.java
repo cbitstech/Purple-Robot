9e8de79cee515e9d4121b7a9a60b5d8c61adc30f
Chris Karr <github@audacious-software.com>
2013-04-30 15:21:04 -0500

    Call log statistics probe.

---
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/features/CallHistoryFeature.java
@@ -0,0 +1,259 @@
+package edu.northwestern.cbits.purple_robot_manager.probes.features;
+
+import java.util.ArrayList;
+
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.CallLog;
+import edu.northwestern.cbits.purple_robot_manager.R;
+
+public class CallHistoryFeature extends Feature
+{
+	private static final String TOTAL = "TOTAL";
+	private static final String OUTCOMING_COUNT = "OUTGOING_COUNT";
+	private static final String STRANGER_COUNT = "STRANGER_COUNT";
+	private static final String ACQUAINTANCE_RATIO = "ACQUAINTANCE_RATIO";
+	private static final String AVG_DURATION = "AVG_DURATION";
+	private static final String INCOMING_COUNT = "INCOMING_COUNT";
+	private static final String ACQUAINTANCE_COUNT = "ACQUIANTANCE_COUNT";
+	private static final String INCOMING_RATIO = "INCOMING_RATIO";
+	private static final String TOTAL_DURATION = "TOTAL_DURATION";
+	private static final String MAX_DURATION = "MAX_DURATION";
+	private static final String STD_DEVIATION = "STD_DEVIATION";
+	private static final String MIN_DURATION = "MIN_DURATION";
+	private static final String WINDOW_SIZE = "WINDOW_SIZE";
+	private static final String WINDOWS = "WINDOWS";
+	private static final String ACK_COUNT = "ACK_COUNT";
+	private static final String NEW_COUNT = "NEW_COUNT";
+	private static final String ACK_RATIO = "ACK_RATIO";
+
+	private long _lastCheck = 0;
+
+	protected String featureKey() 
+	{
+		return "call_history";
+	}
+
+	public String name(Context context) 
+	{
+		return "edu.northwestern.cbits.purple_robot_manager.probes.features.CallHistoryFeature";
+	}
+
+	public String title(Context context) 
+	{
+		return context.getString(R.string.title_call_history_feature);
+	}
+
+	public String summary(Context context)
+	{
+		return context.getString(R.string.summary_call_history_feature_desc);
+	}
+
+	public void enable(Context context) 
+	{
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		
+		Editor e = prefs.edit();
+		e.putBoolean("config_feature_call_history_enabled", true);
+		
+		e.commit();
+	}		
+
+	public boolean isEnabled(final Context context)
+	{
+		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+		final long now = System.currentTimeMillis();
+
+		if (super.isEnabled(context))
+		{
+			if (prefs.getBoolean("config_feature_call_history_enabled", true))
+			{
+				synchronized(this)
+				{
+					if (now - this._lastCheck > 15 * 60 * 1000)
+					{
+						this._lastCheck = now;
+
+						Bundle bundle = new Bundle();
+						bundle.putString("PROBE", this.name(context));
+						bundle.putLong("TIMESTAMP", System.currentTimeMillis() / 1000);
+
+						ArrayList<Bundle> analyses = new ArrayList<Bundle>();
+
+						double[] periods = { 0.25, 0.5, 1.0, 4.0, 12.0, 24.0, 168.0 };
+							
+						for (double period : periods)
+						{
+							Bundle analysis = new Bundle();
+
+							long start = now - ((long) Math.floor(period * 1000 * 60 * 60));
+
+							double total = 0;
+							double incomingCount = 0;
+							double acquaintanceCount = 0;
+							double ackCount = 0;
+							
+							ArrayList<Double> durations = new ArrayList<Double>();
+
+							String selection = "date > ?";
+							String[] selectionArgs = { "" + start };
+							
+							Cursor cursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, null, selection, selectionArgs, null);
+
+							while (cursor.moveToNext())
+							{
+								total += 1;
+								
+								for (int i = 0; i < cursor.getColumnCount(); i++)
+								{
+									String name = cursor.getColumnName(i);
+									
+									Object value = cursor.getString(i);
+									
+									if (name.equals(CallLog.Calls.TYPE))
+									{
+										int type = cursor.getInt(i);
+										
+										if (type == CallLog.Calls.INCOMING_TYPE)
+											incomingCount += 1;
+									}
+									else if (name.equals(CallLog.Calls.DURATION))
+										durations.add((double) cursor.getInt(i));
+									else if (name.equals(CallLog.Calls.CACHED_NAME))
+									{
+										if (value == null)
+										{
+											
+										}
+										else
+											acquaintanceCount += 1;
+									}
+									else if (name.equals(CallLog.Calls.NEW))
+									{
+										if (cursor.getInt(i) != 0)
+											ackCount += 1;
+									}
+								}
+							}
+
+							cursor.close();
+							
+							if (total > 0)
+							{
+								analysis.putDouble(CallHistoryFeature.TOTAL, total);
+	
+								analysis.putDouble(CallHistoryFeature.INCOMING_COUNT, incomingCount);
+								analysis.putDouble(CallHistoryFeature.OUTCOMING_COUNT, total - incomingCount);
+								analysis.putDouble(CallHistoryFeature.INCOMING_RATIO, incomingCount / total);
+	
+								analysis.putDouble(CallHistoryFeature.ACQUAINTANCE_COUNT, acquaintanceCount);
+								analysis.putDouble(CallHistoryFeature.STRANGER_COUNT, total - acquaintanceCount);
+								analysis.putDouble(CallHistoryFeature.ACQUAINTANCE_RATIO, acquaintanceCount / total);
+	
+								analysis.putDouble(CallHistoryFeature.ACK_COUNT, ackCount);
+								analysis.putDouble(CallHistoryFeature.NEW_COUNT, total - ackCount);
+								analysis.putDouble(CallHistoryFeature.ACK_RATIO, ackCount / total);
+
+								double maxDuration = Double.MIN_VALUE;
+								double minDuration = Double.MAX_VALUE;
+								
+								double totalDuration = 0;
+								
+								double[] primitives = new double[durations.size()];
+								
+								for (int k = 0; k < durations.size(); k++)
+								{
+									Double duration = durations.get(k);
+									
+									primitives[k] = duration.doubleValue();
+									
+									totalDuration += duration;
+									
+									if (maxDuration < duration)
+										maxDuration = duration;
+									
+									if (minDuration > duration)
+										minDuration = duration;
+								}
+								
+								StandardDeviation sd = new StandardDeviation();
+								double stdDev = sd.evaluate(primitives);
+								
+								analysis.putDouble(CallHistoryFeature.TOTAL_DURATION, totalDuration);
+								analysis.putDouble(CallHistoryFeature.AVG_DURATION, totalDuration / total);
+								analysis.putDouble(CallHistoryFeature.MIN_DURATION, minDuration);
+								analysis.putDouble(CallHistoryFeature.MAX_DURATION, maxDuration);
+								analysis.putDouble(CallHistoryFeature.STD_DEVIATION, stdDev);
+							}
+							else
+							{
+								analysis.putDouble(CallHistoryFeature.TOTAL, 0);
+								
+								analysis.putDouble(CallHistoryFeature.INCOMING_COUNT, 0);
+								analysis.putDouble(CallHistoryFeature.OUTCOMING_COUNT, 0);
+								analysis.putDouble(CallHistoryFeature.INCOMING_RATIO, 0);
+	
+								analysis.putDouble(CallHistoryFeature.ACQUAINTANCE_COUNT, 0);
+								analysis.putDouble(CallHistoryFeature.STRANGER_COUNT, 0);
+								analysis.putDouble(CallHistoryFeature.ACQUAINTANCE_RATIO, 0);
+	
+								analysis.putDouble(CallHistoryFeature.ACK_COUNT, 0);
+								analysis.putDouble(CallHistoryFeature.NEW_COUNT, 0);
+								analysis.putDouble(CallHistoryFeature.ACK_RATIO, 0);
+
+								analysis.putDouble(CallHistoryFeature.TOTAL_DURATION, 0);
+								analysis.putDouble(CallHistoryFeature.AVG_DURATION, 0);
+								analysis.putDouble(CallHistoryFeature.MIN_DURATION, 0);
+								analysis.putDouble(CallHistoryFeature.MAX_DURATION, 0);
+								analysis.putDouble(CallHistoryFeature.STD_DEVIATION, 0);
+							}
+							
+							analysis.putLong(CallHistoryFeature.WINDOW_SIZE, now - start);
+							
+							analyses.add(analysis);
+						}
+						
+						bundle.putParcelableArrayList(CallHistoryFeature.WINDOWS, analyses);
+
+						this.transmitData(context, bundle);
+					}
+
+					return true;
+				}
+			}
+			
+		}
+		
+		return false;
+	}
+
+	public String summarizeValue(Context context, Bundle bundle)
+	{
+		ArrayList<Bundle> windows = bundle.getParcelableArrayList(CallHistoryFeature.WINDOWS);
+		
+		Bundle window = windows.get(windows.size() - 1);
+
+		double avgDuration = window.getDouble(CallHistoryFeature.AVG_DURATION);
+		double total = window.getDouble(CallHistoryFeature.TOTAL);
+		double stdDev = window.getDouble(CallHistoryFeature.STD_DEVIATION);
+
+		return String.format(context.getResources().getString(R.string.summary_call_history_probe), total, avgDuration, stdDev);
+	}
+	
+	public void disable(Context context) 
+	{
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		
+		Editor e = prefs.edit();
+		e.putBoolean("config_feature_call_history_enabled", false);
+		
+		e.commit();
+	}
+}
