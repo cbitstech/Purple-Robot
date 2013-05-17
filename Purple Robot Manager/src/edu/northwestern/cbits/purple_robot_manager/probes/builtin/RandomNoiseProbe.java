3b5091f0fd3fdf2b16b94b18f568abeb9e31a490
Chris Karr <github@audacious-software.com>
2013-05-17 17:10:12 -0500

    Added acknowledgements.

--- Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/builtin/RandomNoiseProbe.java
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/builtin/RandomNoiseProbe.java
@@ -14,7 +14,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
