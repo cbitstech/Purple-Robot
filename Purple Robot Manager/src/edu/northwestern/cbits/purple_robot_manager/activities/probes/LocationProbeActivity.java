1ba71832865870b714743cf29e78028ca06aa00a
Chris Karr <github@audacious-software.com>
2013-09-10 15:12:07 -0500

    Updating location probe activity to new Google Maps API.

---
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/activities/probes/LocationProbeActivity.java
@@ -0,0 +1,92 @@
+package edu.northwestern.cbits.purple_robot_manager.activities.probes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import edu.northwestern.cbits.purple_robot_manager.ProbeViewerActivity;
+import edu.northwestern.cbits.purple_robot_manager.R;
+import edu.northwestern.cbits.purple_robot_manager.db.ProbeValuesProvider;
+import edu.northwestern.cbits.purple_robot_manager.probes.builtin.LocationProbe;
+
+@SuppressLint("SimpleDateFormat")
+public class LocationProbeActivity extends ActionBarActivity
+{
+	protected void onCreate(Bundle savedInstanceState)
+    {
+		super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.layout_location_activity);
+        
+        this.getSupportActionBar().setTitle(R.string.title_location_history);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+		
+		long minTime = Long.MAX_VALUE;
+		long maxTime = Long.MIN_VALUE;
+		
+		Cursor cursor = ProbeValuesProvider.getProvider(this).retrieveValues(this, LocationProbe.DB_TABLE, LocationProbe.databaseSchema());
+
+		while (cursor.moveToNext())
+		{
+			long time = ((long) cursor.getDouble(cursor.getColumnIndex(ProbeValuesProvider.TIMESTAMP))) * 1000;
+			
+			if (time < minTime)
+				minTime = time;
+
+			if (time > maxTime)
+				maxTime = time;
+		}
+		
+		String startDate = sdf.format(new Date(minTime));
+		String endDate = sdf.format(new Date(maxTime));
+
+		String subtitle = startDate + " (" + cursor.getCount() + ")";
+
+		if (!startDate.equals(endDate))
+			subtitle = startDate + " - " + endDate + " (" + cursor.getCount() + ")";
+
+		this.getSupportActionBar().setSubtitle(subtitle);
+
+		cursor.close();
+    }
+	
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.menu_probe_activity, menu);
+        
+        MenuItem refresh = menu.findItem(R.id.menu_refresh);
+        MenuItem label = menu.findItem(R.id.menu_new_label);
+        
+        refresh.setVisible(false);
+        label.setVisible(false);
+        
+        return true;
+	}
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+    	{
+    		case R.id.menu_data_item:
+				Intent dataIntent = new Intent(this, ProbeViewerActivity.class);
+
+				dataIntent.putExtra("probe_name", this.getIntent().getStringExtra("probe_name"));
+				dataIntent.putExtra("probe_bundle", this.getIntent().getParcelableExtra("probe_bundle"));
+
+				this.startActivity(dataIntent);
+
+    			break;
+    	}
+
+    	return true;
+    }
+}
---
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/activities/probes/LocationProbeFragment.java
@@ -0,0 +1,119 @@
+package edu.northwestern.cbits.purple_robot_manager.activities.probes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.database.Cursor;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+import edu.northwestern.cbits.purple_robot_manager.db.ProbeValuesProvider;
+import edu.northwestern.cbits.purple_robot_manager.probes.builtin.LocationProbe;
+
+public class LocationProbeFragment extends SupportMapFragment  
+{
+	public void onActivityCreated (Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+		
+		try 
+		{
+			MapsInitializer.initialize(this.getActivity());
+		} 
+		catch (GooglePlayServicesNotAvailableException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		List<Location> locations = new ArrayList<Location>();
+
+		Cursor cursor = ProbeValuesProvider.getProvider(this.getActivity()).retrieveValues(this.getActivity(), LocationProbe.DB_TABLE, LocationProbe.databaseSchema());
+
+		while (cursor.moveToNext())
+		{
+			Location l = new Location(this.getClass().getCanonicalName());
+
+			l.setLatitude(cursor.getDouble(cursor.getColumnIndex(LocationProbe.LATITUDE_KEY)));
+			l.setLongitude(cursor.getDouble(cursor.getColumnIndex(LocationProbe.LONGITUDE_KEY)));
+
+			l.setTime(((long) cursor.getDouble(cursor.getColumnIndex(ProbeValuesProvider.TIMESTAMP))) * 1000);
+
+			locations.add(l);
+		}
+
+		cursor.close();
+
+		double minLat = 90;
+		double maxLat = -90;
+		double minLon = 180;
+		double maxLon = -180;
+
+		long minTime = Long.MAX_VALUE;
+		long maxTime = Long.MIN_VALUE;
+
+		for (Location l : locations)
+		{
+			double latitude = l.getLatitude();
+			double longitude = l.getLongitude();
+
+			long time = l.getTime();
+
+			if (latitude < minLat)
+				minLat = latitude;
+
+			if (latitude > maxLat)
+				maxLat = latitude;
+
+			if (longitude < minLon);
+				minLon = longitude;
+
+			if (longitude > maxLon)
+				maxLon = longitude;
+
+			if (time < minTime)
+				minTime = time;
+
+			if (time > maxTime)
+				maxTime = time;
+		}
+		
+		GoogleMap map = this.getMap();
+		map.setIndoorEnabled(true);
+		map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+		LatLng lookAt = new LatLng((minLat + ((maxLat - minLat) / 2)), (minLon + ((maxLon - minLon) / 2)));
+		
+		CameraPosition camera = CameraPosition.fromLatLngZoom(lookAt, 12);
+		map.moveCamera(CameraUpdateFactory.newCameraPosition(camera));
+		
+		for (Location location : locations)
+		{
+			CircleOptions options = new CircleOptions();
+			options.center(new LatLng(location.getLatitude(), location.getLongitude()));
+			options.fillColor(Color.parseColor("#AA66CC"));
+			options.strokeColor(Color.parseColor("#AA66CC"));
+			options.strokeWidth(20.0f);
+			options.radius(5);
+			
+			map.addCircle(options);
+		}
+	}
+
+	protected boolean isRouteDisplayed()
+	{
+		return false;
+	}
+}
