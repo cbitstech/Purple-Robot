f3c2c6d39b367b8035700a9fd579fc37abdf837c
Chris Karr <github@audacious-software.com>
2013-09-16 16:02:18 -0500

    Initial S-expression support.

---
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/util/sexp/List.java
@@ -0,0 +1,39 @@
+package edu.northwestern.cbits.purple_robot_manager.util.sexp;
+
+import java.util.ArrayList;
+
+public class List extends SExpression
+{
+	private ArrayList<SExpression> _elements = new ArrayList<SExpression>();
+	
+	public List()
+	{
+		
+	}
+	
+	public List(java.util.List<SExpression> items)
+	{
+		this._elements.addAll(items);
+	}
+
+	public String toString(boolean pretty) 
+	{
+		// TODO: Obey pretty...
+		
+		StringBuffer sb = new StringBuffer();
+	
+		sb.append("(");
+		
+		for (SExpression sexp : this._elements)
+		{
+			if (sb.length() > 1)
+				sb.append(" ");
+			
+			sb.append(sexp.toString(pretty));
+		}
+
+		sb.append(")");
+
+		return sb.toString();
+	}
+}
