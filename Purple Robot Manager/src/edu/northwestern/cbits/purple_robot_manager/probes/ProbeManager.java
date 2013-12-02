1112c2e1d9ea4d1376cf5f7f34ee1aeb51e8ee29
Chris Karr <github@audacious-software.com>
2013-12-02 06:38:28 -0600

    Facebook probe work

--- Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/ProbeManager.java
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/ProbeManager.java
@@ -31,6 +31,7 @@ import edu.northwestern.cbits.purple_robot_manager.probes.builtin.CommunicationE
 import edu.northwestern.cbits.purple_robot_manager.probes.builtin.CommunicationLogProbe;
 import edu.northwestern.cbits.purple_robot_manager.probes.builtin.ContinuousProbe;
 import edu.northwestern.cbits.purple_robot_manager.probes.builtin.DateCalendarProbe;
+import edu.northwestern.cbits.purple_robot_manager.probes.builtin.FacebookEventsProbe;
 import edu.northwestern.cbits.purple_robot_manager.probes.builtin.FacebookProbe;
 import edu.northwestern.cbits.purple_robot_manager.probes.builtin.GeomagneticRotationProbe;
 import edu.northwestern.cbits.purple_robot_manager.probes.builtin.GravityProbe;
@@ -451,6 +452,13 @@ public class ProbeManager
 			else if (probe instanceof FacebookProbe)
 			{
 				FacebookProbe facebook = (FacebookProbe) probe;
+
+				if (facebook.name(context).equalsIgnoreCase(name))
+					found = true;
+			}
+			else if (probe instanceof FacebookEventsProbe)
+			{
+				FacebookEventsProbe facebook = (FacebookEventsProbe) probe;
 
 				if (facebook.name(context).equalsIgnoreCase(name))
 					found = true;
