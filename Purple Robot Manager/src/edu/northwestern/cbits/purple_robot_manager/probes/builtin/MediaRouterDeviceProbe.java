1c9368b9cd1187f04884e56924aa12b4149ff146
Chris Karr <github@audacious-software.com>
2014-03-28 06:47:06 -0500

    Initial Google Cast / Media Router support.

---
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/probes/builtin/MediaRouterDeviceProbe.java
@@ -0,0 +1,52 @@
+package edu.northwestern.cbits.purple_robot_manager.probes.builtin;
+
+import android.content.Context;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import edu.northwestern.cbits.purple_robot_manager.probes.Probe;
+
+public class MediaRouterDeviceProbe extends Probe {
+
+	@Override
+	public String name(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String title(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String probeCategory(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreferenceScreen preferenceScreen(PreferenceActivity settingsActivity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String summary(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enable(Context context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disable(Context context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
