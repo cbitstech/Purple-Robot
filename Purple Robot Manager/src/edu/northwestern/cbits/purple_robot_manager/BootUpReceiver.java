29e0e8c8fb7f4b1df9cd0e1ec67256e2bfbbf3d6
Chris Karr <github@audacious-software.com>
2014-04-11 16:03:09 -0500

    Sticky notification work.

--- Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/BootUpReceiver.java
+++ Purple Robot Manager/src/edu/northwestern/cbits/purple_robot_manager/BootUpReceiver.java
@@ -1,6 +1,12 @@
 package edu.northwestern.cbits.purple_robot_manager;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.northwestern.cbits.purple_robot_manager.config.LegacyJSONConfigFile;
+import edu.northwestern.cbits.purple_robot_manager.logging.LogManager;
+import edu.northwestern.cbits.purple_robot_manager.scripting.BaseScriptEngine;
+import edu.northwestern.cbits.purple_robot_manager.scripting.JavaScriptEngine;
 import edu.northwestern.cbits.purple_robot_manager.triggers.TriggerManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -30,5 +36,22 @@ public class BootUpReceiver extends BroadcastReceiver
     	LegacyJSONConfigFile.getSharedFile(context.getApplicationContext());
     	
     	TriggerManager.getInstance(context).fireMissedTriggers(context, now);
+    	
+    	if (prefs.contains(BaseScriptEngine.STICKY_NOTIFICATION_PARAMS))
+    	{
+    		try 
+    		{
+				JSONObject json = new JSONObject(prefs.getString(BaseScriptEngine.STICKY_NOTIFICATION_PARAMS, "{}"));
+
+				JavaScriptEngine engine = new JavaScriptEngine(context);
+				
+				engine.showScriptNotification(json.getString("title"), json.getString("message"), json.getBoolean("persistent"), json.getBoolean("sticky"), json.getString("script"));
+			}
+    		catch (JSONException ex) 
+    		{
+    			LogManager.getInstance(context).logException(ex);
+			}
+    		
+    	}
     }
 }
