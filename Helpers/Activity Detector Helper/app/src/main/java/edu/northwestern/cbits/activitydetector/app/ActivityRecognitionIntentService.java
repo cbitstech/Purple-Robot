563573defe5906fb24ae88131041f9c31dbe203c
gwensmu <gwen.smuda@gmail.com>
2014-05-20 12:05:28 -0500

    Initial Commit of Rebuilt Project

---
+++ Helpers/Activity Detector Helper/app/src/main/java/edu/northwestern/cbits/activitydetector/app/ActivityRecognitionIntentService.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.northwestern.cbits.activitydetector.app;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.TextView;
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Service that receives ActivityRecognition updates. It receives updates
+ * in the background, even if the main Activity is not visible.
+ */
+public class ActivityRecognitionIntentService extends IntentService {
+
+    public static String ACTIVITY_NAME = "unknown";
+
+    // Store the app's shared preferences repository
+    private SharedPreferences mPrefs;
+
+    public ActivityRecognitionIntentService() {
+        // Set the label for the service's background thread
+        super("ActivityRecognitionIntentService");
+    }
+
+    @Override
+    public void onHandleIntent(Intent intent) {
+
+        if (ActivityRecognitionResult.hasResult(intent)) {
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+
+            // Get the most probable activity from the list of activities in the update
+            DetectedActivity mostProbableActivity = result.getMostProbableActivity();
+
+            // Get the type of activity
+            int activityType = mostProbableActivity.getType();
+
+            // Get human-readable name
+            String activityName = getNameFromType(activityType);
+
+            ACTIVITY_NAME = activityName;
+        }
+    }
+
+    /**
+     * Get a content Intent for the notification
+     *
+     * @return A PendingIntent that starts the device's Location Settings panel.
+     */
+    private PendingIntent getContentIntent() {
+
+        // Set the Intent action to open Location Settings
+        Intent gpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+
+        // Create a PendingIntent to start an Activity
+        return PendingIntent.getActivity(getApplicationContext(), 0, gpsIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    /**
+     * Map detected activity types to strings
+     *
+     * @param activityType The detected activity type
+     * @return A user-readable name for the type
+     */
+    private String getNameFromType(int activityType) {
+        switch(activityType) {
+            case DetectedActivity.IN_VEHICLE:
+                return "in_vehicle";
+            case DetectedActivity.ON_BICYCLE:
+                return "on_bicycle";
+            case DetectedActivity.ON_FOOT:
+                return "on_foot";
+            case DetectedActivity.STILL:
+                return "still";
+            case DetectedActivity.UNKNOWN:
+                return "unknown";
+            case DetectedActivity.TILTING:
+                return "tilting";
+        }
+        return "unknown";
+    }
+}
