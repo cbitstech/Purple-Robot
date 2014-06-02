9b261da7d0ce09f5a829c7a36d3eb6c50697053e
Chris Karr <github@audacious-software.com>
2014-06-02 18:10:51 -0500

    Supporting files for D3.

---
+++ Purple Robot Manager/assets/webkit/vendor/spline_template.js
@@ -0,0 +1,52 @@
+{
+	chart:
+	{
+		renderTo: "container",
+		type: "spline"
+	},
+	scrollbar:
+	{
+		enabled: true
+	},
+	title:
+	{
+		text: ""
+	},
+	yAxis:
+	{
+		title:
+		{
+			text: ""
+		}
+	},
+	legend:
+	{
+		enabled: false
+	},
+	credits:
+	{
+		enabled: false
+	},
+	plotOptions:
+	{
+		spline:
+		{
+			shadow: false,
+			marker:
+			{
+				enabled: false,
+				states:
+				{
+					hover:
+					{
+						enabled: true,
+						symbol: "circle",
+						radius: 5,
+						lineWidth: 1
+					}
+				}
+			}
+		}
+	},
+	series: []
+ }
