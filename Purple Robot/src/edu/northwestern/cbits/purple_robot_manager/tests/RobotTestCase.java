d99d93889de2a7a6ab315a6603bde7a2564acde6
Chris Karr <github@audacious-software.com>
2014-08-13 11:53:38 -0500

    Added user ID test case and introduced intentional delay into user id mutation calls.

--- Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/tests/RobotTestCase.java
+++ Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/tests/RobotTestCase.java
@@ -1,14 +1,55 @@
 package edu.northwestern.cbits.purple_robot_manager.tests;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.net.http.AndroidHttpClient;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.test.AndroidTestCase;
+import android.util.Log;
+
 import edu.northwestern.cbits.purple_robot_manager.R;
 import edu.northwestern.cbits.purple_robot_manager.activities.TestActivity;
+import edu.northwestern.cbits.purple_robot_manager.logging.LiberalSSLSocketFactory;
+import edu.northwestern.cbits.purple_robot_manager.plugins.DataUploadPlugin;
 
 public abstract class RobotTestCase extends AndroidTestCase 
 {
@@ -93,5 +134,88 @@ public abstract class RobotTestCase extends AndroidTestCase
 			return 1;
 		
 		return this.name(context).compareToIgnoreCase(other.name(context));
+	}
+	
+	public String syncHttpPost(String url, Map<String, String> payload) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, KeyManagementException, UnrecoverableKeyException, URISyntaxException
+	{
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this._context);
+
+		AndroidHttpClient androidClient = AndroidHttpClient.newInstance("Purple Robot", this._context);
+
+        HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+
+		SchemeRegistry registry = new SchemeRegistry();
+		registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+			
+		SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
+			
+		if (prefs.getBoolean(DataUploadPlugin.ALLOW_ALL_SSL_CERTIFICATES, DataUploadPlugin.ALLOW_ALL_SSL_CERTIFICATES_DEFAULT))
+		{
+	        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+	        trustStore.load(null, null);
+
+	        socketFactory = new LiberalSSLSocketFactory(trustStore);								
+		}
+
+		registry.register(new Scheme("https", socketFactory, 443));
+		
+		HttpParams params = androidClient.getParams();
+		HttpConnectionParams.setConnectionTimeout(params, 180000);
+		HttpConnectionParams.setSoTimeout(params, 180000);
+		
+		SingleClientConnManager mgr = new SingleClientConnManager(params, registry);
+		HttpClient httpClient = new DefaultHttpClient(mgr, params);
+
+		HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
+
+		String body = null;
+
+		URI siteUri = new URI(url);
+		
+		HttpPost httpPost = new HttpPost(siteUri);
+		httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+		
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		
+		for (String key : payload.keySet())
+			nameValuePairs.add(new BasicNameValuePair(key, payload.get(key)));
+
+		HttpEntity entity = new UrlEncodedFormEntity(nameValuePairs, HTTP.UTF_8);
+
+		httpPost.setEntity(entity);
+
+		HttpResponse response = httpClient.execute(httpPost);
+
+		HttpEntity httpEntity = response.getEntity();
+
+		String contentHeader = null;
+
+		if (response.containsHeader("Content-Encoding"))
+			contentHeader = response.getFirstHeader("Content-Encoding").getValue();
+
+		if (contentHeader != null && contentHeader.endsWith("gzip"))
+		{
+			BufferedInputStream in = new BufferedInputStream(AndroidHttpClient.getUngzippedContent(httpEntity));
+
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+			int read = 0;
+			byte[] buffer = new byte[1024];
+
+			while ((read = in.read(buffer, 0, buffer.length)) != -1)
+				out.write(buffer, 0, read);
+
+			in.close();
+
+			body = out.toString("UTF-8");
+		}
+		else
+			body = EntityUtils.toString(httpEntity);
+
+		androidClient.close();
+		
+		Log.e("PR", "BODY: " + body + " " + response.getStatusLine());
+		
+		return body;
 	}
 }
---
+++ Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/tests/UserIdTestCase.java
@@ -0,0 +1,206 @@
+package edu.northwestern.cbits.purple_robot_manager.tests;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import junit.framework.Assert;
+import android.content.Context;
+
+import edu.northwestern.cbits.purple_robot_manager.R;
+import edu.northwestern.cbits.purple_robot_manager.scripting.JavaScriptEngine;
+
+public class UserIdTestCase extends RobotTestCase 
+{
+	private static final String TEST_ID_1 = "test1@example.com";
+	private static final String TEST_ID_2 = "test2@example.com";
+
+	public UserIdTestCase(Context context, int priority) 
+	{
+		super(context, priority);
+	}
+	
+	public void test() 
+	{
+		if (this.isSelected(this._context) == false)
+			return;
+
+		JavaScriptEngine engine = new JavaScriptEngine(this._context);
+
+		String originalId = engine.fetchUserId();
+
+		try 
+		{
+			this.broadcastUpdate("Testing code directly...");
+			
+			Assert.assertNotNull("UID1", originalId);
+
+			Thread.sleep(1000);
+			
+			engine.setUserId(UserIdTestCase.TEST_ID_1);
+
+			Thread.sleep(1000);
+			
+			Assert.assertEquals("UID2", UserIdTestCase.TEST_ID_1, engine.fetchUserId());
+
+			engine.setUserId(UserIdTestCase.TEST_ID_2);
+
+			Thread.sleep(1000);
+			
+			Assert.assertEquals("UID3", UserIdTestCase.TEST_ID_2, engine.fetchUserId());
+			
+			this.broadcastUpdate("Testing over HTTP API... (1/3)");
+
+			JSONObject command = new JSONObject();
+			command.put("command", "execute_script");
+			command.put("script", "PurpleRobot.setUserId('" + UserIdTestCase.TEST_ID_1 + "');");
+			
+			HashMap<String, String> payload = new HashMap<String, String>();
+			payload.put("json", command.toString(2));
+			
+			String response = this.syncHttpPost("http://127.0.0.1:12345/json/submit", payload);
+			
+			Assert.assertNotNull("UID4", response);
+			
+			JSONObject responseJson = new JSONObject(response);
+			
+			Assert.assertEquals("UID5", "ok", responseJson.get("status"));
+
+			Thread.sleep(1000);
+
+			command.put("script", "PurpleRobot.fetchUserId();");
+			
+			payload.put("json", command.toString(2));
+			
+			response = this.syncHttpPost("http://127.0.0.1:12345/json/submit", payload);
+			
+			Assert.assertNotNull("UID6", response);
+			
+			responseJson = new JSONObject(response);
+			
+			Assert.assertEquals("UID7", "ok", responseJson.get("status"));
+			Assert.assertEquals("UID8", UserIdTestCase.TEST_ID_1, responseJson.get("payload"));
+
+			this.broadcastUpdate("Testing over HTTP API... (2/3)");
+
+			Thread.sleep(1000);
+
+			command.put("script", "PurpleRobot.setUserId('" + UserIdTestCase.TEST_ID_2 + "');");
+			
+			payload.put("json", command.toString(2));
+			
+			response = this.syncHttpPost("http://127.0.0.1:12345/json/submit", payload);
+			
+			Assert.assertNotNull("UID9", response);
+			
+			responseJson = new JSONObject(response);
+			
+			Assert.assertEquals("UID10", "ok", responseJson.get("status"));
+
+			Thread.sleep(1000);
+
+			command.put("script", "PurpleRobot.fetchUserId();");
+			
+			payload.put("json", command.toString(2));
+			
+			response = this.syncHttpPost("http://127.0.0.1:12345/json/submit", payload);
+			
+			Assert.assertNotNull("UID11", response);
+			
+			responseJson = new JSONObject(response);
+			
+			Assert.assertEquals("UID12", "ok", responseJson.get("status"));
+			Assert.assertEquals("UID13", UserIdTestCase.TEST_ID_2, responseJson.get("payload"));
+
+			Thread.sleep(1000);
+
+			this.broadcastUpdate("Testing over HTTP API... (3/3)");
+
+			command.put("command", "execute_script");
+			command.put("script", "PurpleRobot.setUserId('" + UserIdTestCase.TEST_ID_1 + "');");
+			
+			payload.put("json", command.toString(2));
+			
+			response = this.syncHttpPost("http://127.0.0.1:12345/json/submit", payload);
+			
+			Assert.assertNotNull("UID14", response);
+			
+			responseJson = new JSONObject(response);
+			
+			Assert.assertEquals("UID15", "ok", responseJson.get("status"));
+
+			Thread.sleep(1000);
+
+			command.put("script", "PurpleRobot.fetchUserId();");
+			
+			payload.put("json", command.toString(2));
+			
+			response = this.syncHttpPost("http://127.0.0.1:12345/json/submit", payload);
+			
+			Assert.assertNotNull("UID16", response);
+			
+			responseJson = new JSONObject(response);
+			
+			Assert.assertEquals("UID17", "ok", responseJson.get("status"));
+			Assert.assertEquals("UID18", UserIdTestCase.TEST_ID_1, responseJson.get("payload"));
+		} 
+		catch (InterruptedException e) 
+		{
+			Assert.fail("UID100");
+		} 
+		catch (JSONException e) 
+		{
+			Assert.fail("UID101");
+		} 
+		catch (KeyManagementException e) 
+		{
+			Assert.fail("UID102");
+		} 
+		catch (UnrecoverableKeyException e) 
+		{
+			Assert.fail("UID103");
+		} 
+		catch (KeyStoreException e) 
+		{
+			Assert.fail("UID104");
+		} 
+		catch (NoSuchAlgorithmException e) 
+		{
+			Assert.fail("UID105");
+		} 
+		catch (CertificateException e) 
+		{
+			Assert.fail("UID106");
+		} 
+		catch (IOException e) 
+		{
+			Assert.fail("UID107");
+		} 
+		catch (URISyntaxException e) 
+		{
+			Assert.fail("UID108");
+		}
+		
+		engine.setUserId(originalId, true);
+
+		Assert.assertEquals("UID1000", originalId, engine.fetchUserId());
+	}
+	
+	public int estimatedMinutes() 
+	{
+		return 1;
+	}
+
+	public String name(Context context) 
+	{
+		return context.getString(R.string.name_user_id_test);
+	}
+}
