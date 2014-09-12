f55cbedd5756a2398a59d7c20751ddfd0c7095d6
Chris Karr <github@audacious-software.com>
2014-09-12 17:29:49 -0500

    Added lenient SSL checks to readUrl script call.

--- Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/scripting/BaseScriptEngine.java
+++ Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/scripting/BaseScriptEngine.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,6 +15,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -198,8 +207,52 @@ public abstract class BaseScriptEngine
 
 	public String readUrl(String urlString)
 	{
+		return this.readUrl(urlString, false);
+	}
+
+	public String readUrl(String urlString, boolean lenient)
+	{
 		try 
 		{
+			if (lenient)
+			{
+				TrustManager[] trustAllCerts = new TrustManager[] 
+				{
+					new X509TrustManager() 
+					{
+						public java.security.cert.X509Certificate[] getAcceptedIssuers() 
+						{
+							return null;
+						}
+					
+						public void checkClientTrusted(X509Certificate[] certs, String authType) 
+						{
+						}
+					
+						public void checkServerTrusted(X509Certificate[] certs, String authType) 
+						{
+						}
+					}
+				};
+	
+				SSLContext sc = SSLContext.getInstance("SSL");
+				sc.init(null, trustAllCerts, new java.security.SecureRandom());
+				HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	
+				// Create all-trusting host name verifier
+				HostnameVerifier allHostsValid = new HostnameVerifier() 
+				{
+					public boolean verify(String hostname, SSLSession session) 
+					{
+						// TODO Auto-generated method stub
+						return true;
+					}
+				};
+	
+				// Install the all-trusting host verifier
+				HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+			}
+			
 			URL url = new URL(urlString);
 
 	        URLConnection connection = url.openConnection();
