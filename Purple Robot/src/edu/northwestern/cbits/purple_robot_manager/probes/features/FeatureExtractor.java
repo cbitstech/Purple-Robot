bc5d821d51bfd5a0c7decbd4f65ceb9f02cd2f33
sosata <sosata@gmail.com>
2014-09-16 17:55:17 -0500

    gyro + moments added

--- Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/probes/features/FeatureExtractor.java
+++ Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/probes/features/FeatureExtractor.java
@@ -12,98 +12,193 @@ public class FeatureExtractor {
     
 
     private long window_size;
-    private String[] feature_list;
-    private float[] values;
+    private List<String> feature_list;
+    //private double[] features;
     private boolean hasFFT = false;
     private boolean hasDiff = false;
+    private int dim;
 
-    private List<float[]> signal_diff;
+    private List<double[]> signal_diff;
 
     private FastFourierTransformer fft;
 
-    public FeatureExtractor(long window_size, String[] feature_list) {
+    public FeatureExtractor(long window_size, List<String> feature_list, int dim) {
 
         this.window_size = window_size;
         this.feature_list = feature_list;
+        this.dim = dim;
 
         for (String s: feature_list) {
-            if (s.startsWith("fft")) {
+
+            if ((!hasFFT)&&(s.contains("fft"))) {
                 hasFFT = true;
                 fft = new FastFourierTransformer(DftNormalization.STANDARD);
             }
-            if (s.startsWith("diff")) {
+            if ((!hasDiff)&&(s.contains("diff")))
                 hasDiff = true;
-                signal_diff = new ArrayList<float[]>();
-            }
         }
 
     }
 
-    public float[] ExtractFeatures(Clip clip) {
-
-        if (hasDiff)
-            signal_diff = getDiff(clip.value);
-/*
-        if (hasFFT) {
-            Complex[] fft_values = fft.transform(signal, TransformType.FORWARD);
+    public List<Double> ExtractFeatures(Clip clip) {
+        
+        //Calculating the statistical moments
+        double[] mean = new double[dim];
+        double[] std = new double[dim];
+        double[] skewness = new double[dim];
+        double[] kurtosis = new double[dim];
+        for (int i=0; i<dim; i++) {
+            double[] moments = getMoments(clip.value, i);
+            mean[i] = moments[0];
+            std[i] = moments[1]; 
+            skewness[i] = moments[2];
+            kurtosis[i] = moments[3];
         }
+
+        double[] diff_mean = new double[dim];
+        double[] diff_std = new double[dim];
+        double[] diff_skewness = new double[dim];
+        double[] diff_kurtosis = new double[dim];
+        if (hasDiff) {
+            signal_diff = new ArrayList<double[]>();
+            signal_diff = getDiff(clip.value);
+            //Calculating the statistical moments of the difference signal
+            for (int i=0; i<dim; i++) {
+                double[] moments = getMoments(signal_diff, i);
+                diff_mean[i] = moments[0];
+                diff_std[i] = moments[1]; 
+                diff_skewness[i] = moments[2];
+                diff_kurtosis[i] = moments[3];
+            }
+
+        }
+/*
+        if (hasFFT)
+            Complex[] fft_values = fft.transform(signal, TransformType.FORWARD);
 */
-        values = new float[this.feature_list.length];
+        List<Double> features = new ArrayList<Double>();
 
-        for (int i=0; i<this.feature_list.length; i++) {
+        int i=0;
 
-            switch (feature_list[i]) {
+        for (String s: feature_list) {
 
-                case "nsamp":
-                    values[i] = clip.value.size();
+            switch (s) {
+             
+                case "nsamp": //for debugging purpose. can be removed later
+                    features.add((double)clip.value.size());
                     break;
 
-                case "mean":
-                case "meanx":
-                    values[i] = getMean(clip.value, 0);
+                case "_mean":
+                case "x_mean":
+                    features.add(mean[0]);
+                    break;
+                case "y_mean":
+                    features.add(mean[1]);
+                    break;
+                case "z_mean":
+                    features.add(mean[2]);
                     break;
 
-                case "meany":
-                    values[i] = getMean(clip.value, 1);
+                case "_std":
+                case "x_std":
+                    features.add(std[0]);
+                    break;
+                case "y_std":
+                    features.add(std[1]);
+                    break;
+                case "z_std":
+                    features.add(std[2]);
                     break;
 
-                case "meanz":
-                    values[i] = getMean(clip.value, 2);
+                case "_skew":
+                case "x_skew":
+                    features.add(skewness[0]);
+                    break;
+                case "y_skew":
+                    features.add(skewness[1]);
+                    break;
+                case "z_skew":
+                    features.add(skewness[2]);
                     break;
 
-                case "std":
-                case "stdx":
-                    values[i] = getStd(clip.value, 0);
+                case "_kurt":
+                case "x_kurt":
+                    features.add(kurtosis[0]);
+                    break;
+                case "y_kurt":
+                    features.add(kurtosis[1]);
+                    break;
+                case "z_kurt":
+                    features.add(kurtosis[2]);
                     break;
 
-                case "stdy":
-                    values[i] = getStd(clip.value, 1);
+                case "_diff_mean":
+                case "x_diff_mean":
+                    features.add(diff_mean[0]);
+                    break;
+                case "y_diff_mean":
+                    features.add(diff_mean[1]);
+                    break;
+                case "z_diff_mean":
+                    features.add(diff_mean[2]);
                     break;
 
-                case "stdz":
-                    values[i] = getStd(clip.value, 2);
+                case "_diff_std":
+                case "x_diff_std":
+                    features.add(diff_std[0]);
                     break;
+                case "y_diff_std":
+                    features.add(diff_std[1]);
+                    break;
+                case "z_diff_std":
+                    features.add(diff_std[2]);
+                    break;
+
+                case "_diff_skew":
+                case "x_diff_skew":
+                    features.add(diff_skewness[0]);
+                    break;
+                case "y_diff_skew":
+                    features.add(diff_skewness[1]);
+                    break;
+                case "z_diff_skew":
+                    features.add(diff_skewness[2]);
+                    break;
+
+                case "_diff_kurt":
+                case "x_diff_kurt":
+                    features.add(diff_kurtosis[0]);
+                    break;
+                case "y_diff_kurt":
+                    features.add(diff_kurtosis[1]);
+                    break;
+                case "z_diff_kurt":
+                    features.add(diff_kurtosis[2]);
+                    break;
+
 
                 default:
 
             }
 
+            i++;
+
         }
 
-        return values;
+        return features;
 
     }
 
-    private List<float[]> getDiff(List<float[]> signal) {
+    private List<double[]> getDiff(List<double[]> signal) {
 
-        List<float[]> signal_diff = new ArrayList<float[]>();
+        List<double[]> signal_diff = new ArrayList<double[]>();
 
         for (int i=0; i<signal.size()-1; i++) {
 
-            float[] sig = signal.get(i);
-            float[] sig_next = signal.get(i+1);
+            double[] sig = signal.get(i);
+            double[] sig_next = signal.get(i+1);
 
-            float[] sig_diff = new float[sig.length];
+            double[] sig_diff = new double[sig.length];
 
             for (int j=0; j<sig.length; j++)
                 sig_diff[j] = sig_next[j] - sig[j];
@@ -116,29 +211,58 @@ public class FeatureExtractor {
 
     }
 
-    private float getMean(List<float[]> signal, int axis) {
-        
-        float sum = 0.0f;
-        for (float[] value : signal)
-            sum += value[axis];
-        return sum/signal.size();
+    // This method will calculate mean, standard deviation, skewness, and kurtosis.
+    // each member of the list is one statistical moment, which consists of an array, with
+    // each element accounting for one dimension
 
-    }
+    private double[] getMoments(List<double[]> signal, int axis) {
 
-    private float getStd(List<float[]> signal, int axis) {
-        
-        float mean = 0f;
-        for (float[] value : signal)
-            mean += value[axis];
-        mean /= signal.size();
-        
-        float std = 0f;
-        for (float[] value : signal)
-            std = std + (value[axis]-mean)*(value[axis]-mean);
-        std /= signal.size()-1; //unbiased estimator
-        std = (float)Math.sqrt(std);
+        double N = (double)signal.size();
 
-        return std;
+        if (N<2) {
+            double[] out = {0, 0, 0, 0};
+            return out;
+        }
+
+        double sum = 0f;
+        // for some reason, the following commented-out code generates "concurrent modification exception"!
+        //for (double[] value : signal)
+            //sum += value[axis];
+        for (int i=0; i<N; i++)
+            sum += signal.get(i)[axis];
+            
+        
+        double mean = sum/N;
+
+        double m2 = 0f;
+        double m3 = 0f;
+        double m4 = 0f;
+        double t2,t3,t4;
+        
+        for (int i=0; i<N; i++) {
+
+            t2 = (signal.get(i)[axis]-mean)*(signal.get(i)[axis]-mean);
+            m2 += t2;
+
+            t3 = t2*(signal.get(i)[axis]-mean);
+            m3 += t3;
+
+            t4 = t3*(signal.get(i)[axis]-mean);
+            m4 += t4;
+        }
+
+        double std = (double)Math.sqrt(m2/(N-1)); //unbiased
+        
+        m2 /= N;
+        m3 /= N;
+        m4 /= N;
+ 
+        double skewness = m3/(std*std*std); //unbiased
+
+        double kurtosis = m4/(m2*m2) - 3; //unbiased
+
+        double out[] = {mean, std, skewness, kurtosis};
+        return out;
 
     }
 
