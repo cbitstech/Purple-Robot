3fec88b572055a7c16ecd793753a66fb36d78f3b
Chris J. Karr <github@audacious-software.com>
2014-12-03 08:26:24 -0600

    Purple Watch / Pebble test updates

--- Helpers/Purple Watch/src/edu/northwestern/cbits/purplewatch/DataReceiver.java
+++ Helpers/Purple Watch/src/edu/northwestern/cbits/purplewatch/DataReceiver.java
@@ -8,32 +8,43 @@ import android.util.Log;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.PebbleKit.PebbleDataLogReceiver;
-import com.google.common.primitives.UnsignedInteger;
 
-public class DataReceiver extends PebbleDataLogReceiver 
+public class DataReceiver extends PebbleDataLogReceiver
 {
-	private static boolean _inited = false;
-	
-	protected DataReceiver(UUID uuid) 
-	{
-		super(uuid);
-	}
-	
-	public void onReceive (Context context, Intent intent)
-	{
-		if (DataReceiver._inited)
-			return;
-		
-		Log.e("PW", "INITING RECEIVER");
-		DataReceiver receiver = new DataReceiver(UUID.fromString("3cab0453-ff04-4594-8223-fa357112c305"));
-		
-		PebbleKit.registerDataLogReceiver(context, receiver);
-		
-		super.onReceive(context, intent);
-	}
+    private static boolean _inited = false;
 
-    public void receiveData(final Context context, UUID logUuid, final UnsignedInteger timestamp, final UnsignedInteger tag, final byte[] data) 
+    protected DataReceiver(UUID uuid)
     {
-    	Log.e("PR", "GOT DATA: " + data.length);
+        super(uuid);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        if (DataReceiver._inited)
+            return;
+
+        Log.e("PW", "INITING RECEIVER");
+        DataReceiver receiver = new DataReceiver(UUID.fromString("3cab0453-ff04-4594-8223-fa357112c305"));
+
+        PebbleKit.registerDataLogReceiver(context, receiver);
+
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void receiveData(final Context context, UUID logUuid, final Long timestamp, final Long tag, final byte[] data)
+    {
+        Log.e("PR", "GOT DATA: " + data.length);
+    }
+
+    @Override
+    public void onFinishSession(Context context, UUID logUuid, Long timestamp, Long tag)
+    {
+        super.onFinishSession(context, logUuid, timestamp, tag);
+
+        Log.e("PW", "FINISHED");
+
+        // Session is finished, use the data!
     }
 }
--- Helpers/Purple Watch/src/edu/northwestern/cbits/purplewatch/MainActivity.java
+++ Helpers/Purple Watch/src/edu/northwestern/cbits/purplewatch/MainActivity.java
@@ -1,65 +1,109 @@
 package edu.northwestern.cbits.purplewatch;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.util.List;
+import java.util.UUID;
+
+import org.json.JSONException;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
-public class MainActivity extends ActionBarActivity {
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.PebbleKit.PebbleDataLogReceiver;
+
+public class MainActivity extends ActionBarActivity
+{
+    private PebbleDataLogReceiver _receiver = null;
+
+    private static UUID WATCHAPP_UUID = UUID.fromString("3cab0453-ff04-4594-8223-fa357112c305");
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onResume()
+    {
+        super.onResume();
+
+        Log.e("PW", "ON RESUME " + PebbleKit.isDataLoggingSupported(this));
+
+        PebbleKit.startAppOnPebble(this, WATCHAPP_UUID);
+
+        if (this._receiver == null)
+        {
+            Log.e("PW", "CREATING RECV");
+
+            this._receiver = new PebbleDataLogReceiver(WATCHAPP_UUID)
+            {
+                @Override
+                public void onReceive(final Context context, final Intent intent)
+                {
+                    super.onReceive(context, intent);
+
+                    Log.e("PW", "INTENT: " + intent);
+                }
+
+                @Override
+                public void receiveData(final Context context, UUID logUuid, final Long timestamp, final Long tag,
+                        final byte[] data)
+                {
+                    Log.e("PW", "GOT DATA: " + data.length);
+
+                    List<AccelData> accels = AccelData.fromDataArray(data);
+
+                    for (AccelData accel : accels)
+                    {
+                        try
+                        {
+                            Log.e("PW", "A: " + accel.toJson().toString(2));
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                @Override
+                public void onFinishSession(Context context, UUID logUuid, Long timestamp, Long tag)
+                {
+                    super.onFinishSession(context, logUuid, timestamp, tag);
+
+                    Log.e("PW", "FINISHED");
+                }
+
+                @Override
+                public void receiveData(final Context context, UUID logUuid, final Long timestamp, final Long tag,
+                        final int data)
+                {
+                    Log.e("PW", "TS: " + timestamp + " TAG: " + tag + " DATA: " + data);
+                }
+
+            };
         }
-        return super.onOptionsItemSelected(item);
+
+        PebbleKit.registerDataLogReceiver(this, this._receiver);
+        PebbleKit.requestDataLogsForApp(this, WATCHAPP_UUID);
+        // this.registerReceiver(this._receiver, filter);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
 
-        public PlaceholderFragment() {
-        }
+        Log.e("PW", "UNREGISTER RECV");
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+        PebbleKit.closeAppOnPebble(this, WATCHAPP_UUID);
+
+        this.unregisterReceiver(this._receiver);
     }
-
 }
