542c68d1b8087e8f8dc07e28c1555c674dd92358
Chris J. Karr <github@audacious-software.com>
2015-03-20 08:06:38 -0500

    Wrapped up work on time point marker

--- Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/activities/MarkTimeActivity.java
+++ Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/activities/MarkTimeActivity.java
@@ -1,8 +1,12 @@
 package edu.northwestern.cbits.purple_robot_manager.activities;
 
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
@@ -17,21 +21,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager.LayoutParams;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 
 import edu.northwestern.cbits.purple_robot_manager.R;
+import edu.northwestern.cbits.purple_robot_manager.config.LegacyJSONConfigFile;
+import edu.northwestern.cbits.purple_robot_manager.logging.LogManager;
+import edu.northwestern.cbits.purple_robot_manager.scripting.JavaScriptEngine;
 
 public class MarkTimeActivity extends ActionBarActivity
 {
@@ -48,6 +62,7 @@ public class MarkTimeActivity extends ActionBarActivity
 
         getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         this.getSupportActionBar().setTitle(R.string.title_mark_time);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         this.loadTimestamps();
     }
@@ -111,10 +126,14 @@ public class MarkTimeActivity extends ActionBarActivity
                 Vibrator vibrator = (Vibrator) me.getSystemService(Context.VIBRATOR_SERVICE);
                 vibrator.vibrate(spec, -1);
 
+                if (nameValue.trim().length() == 0)
+                    nameValue = me.getString(R.string.value_anonymous_timestamp);
+
                 me._timestamps.append(now, nameValue);
-                
-                // TODO: XMIT                
-                
+
+                JavaScriptEngine js = new JavaScriptEngine(me);
+                js.emitReading(LegacyJSONConfigFile.toSlug(nameValue), nameValue);
+
                 me.saveTimestamps();
                 me.refreshList();
             }
@@ -132,13 +151,20 @@ public class MarkTimeActivity extends ActionBarActivity
         list.setStackFromBottom(true);
 
         final ArrayList<Long> indices = new ArrayList<Long>();
+        final ArrayList<String> names = new ArrayList<String>();
 
         for (int i = 0; i < this._timestamps.size(); i++)
         {
             indices.add(this._timestamps.keyAt(i));
+
+            String name = this._timestamps.valueAt(i);
+
+            if (names.contains(name) == false)
+                names.add(name);
         }
 
         Collections.sort(indices);
+        Collections.sort(names);
 
         final java.text.DateFormat dateFormat = DateFormat.getMediumDateFormat(this);
         final java.text.DateFormat timeFormat = DateFormat.getTimeFormat(this);
@@ -165,6 +191,50 @@ public class MarkTimeActivity extends ActionBarActivity
         };
 
         list.setAdapter(adapter);
+        list.setEmptyView(this.findViewById(R.id.placeholder_timestamps));
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id)
+            {
+                Date date = new Date(me._timestamps.keyAt(position));
+
+                String dateString = timeFormat.format(date) + " @ " + dateFormat.format(date);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(me);
+                builder.setTitle(R.string.title_clear_timestamp);
+                builder.setMessage(me.getString(R.string.message_clear_timestamp, me._timestamps.valueAt(position), dateString));
+
+                builder.setPositiveButton(R.string.action_clear, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        me._timestamps.removeAt(position);
+                        me.saveTimestamps();
+
+                        me.refreshList();
+                    }
+                });
+
+                builder.setNegativeButton(R.string.action_cancel, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        // Do nothing...
+                    }
+                });
+
+                builder.create().show();
+
+                return true;
+            }
+        });
+
+        final AutoCompleteTextView name = (AutoCompleteTextView) this.findViewById(R.id.text_label_text);
+
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, names);
+        name.setAdapter(namesAdapter);
+        name.setThreshold(1);
     }
 
     private void saveTimestamps()
@@ -206,11 +276,88 @@ public class MarkTimeActivity extends ActionBarActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        final MarkTimeActivity me = this;
+
         final int itemId = item.getItemId();
 
-        if (itemId == R.id.menu_cancel)
+        if (itemId == android.R.id.home)
         {
             this.finish();
+        }
+        else if (itemId == R.id.menu_mail)
+        {
+            StringBuffer message = new StringBuffer();
+            String newline = System.getProperty("line.separator");
+            message.append("Time Point\tTimestamp");
+            message.append(newline);
+
+            for (int i = 0; i < this._timestamps.size(); i++)
+            {
+                message.append(this._timestamps.valueAt(i) + "\t" + this._timestamps.keyAt(i));
+                message.append(newline);
+            }
+
+            try
+            {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_SUBJECT, this.getString(R.string.email_timetamp_subject));
+                intent.putExtra(Intent.EXTRA_TEXT, this.getString(R.string.email_timetamp_message));
+
+                File cacheDir = this.getExternalCacheDir();
+                File configFile = new File(cacheDir, "time-points.txt");
+
+                FileOutputStream fout = new FileOutputStream(configFile);
+
+                fout.write(message.toString().getBytes(Charset.defaultCharset().name()));
+
+                fout.flush();
+                fout.close();
+
+                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(configFile));
+
+                this.startActivity(intent);
+            }
+            catch (ActivityNotFoundException e)
+            {
+                Toast.makeText(this, R.string.toast_mail_not_found, Toast.LENGTH_LONG).show();
+            }
+            catch (FileNotFoundException e)
+            {
+                LogManager.getInstance(this).logException(e);
+            }
+            catch (IOException e)
+            {
+                LogManager.getInstance(this).logException(e);
+            }
+        }
+        else if (itemId == R.id.menu_clear)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.title_clear_timestamps);
+            builder.setMessage(R.string.title_clear_timestamps);
+
+            builder.setPositiveButton(R.string.action_clear, new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    me._timestamps.clear();
+                    me.saveTimestamps();
+
+                    me.refreshList();
+                }
+            });
+
+            builder.setNegativeButton(R.string.action_cancel, new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    // Do nothing...
+                }
+            });
+
+            builder.create().show();
         }
 
         return true;
