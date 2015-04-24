d1841d2817c2f0fa1325f4614b8865a2b5f11c26
Chirs J. Karr <chris@audacious-software.com>
2015-04-23 19:51:02 -0500

    Sanity check for multiple Pebble probes

---
+++ Purple Robot/src/edu/northwestern/cbits/purple_robot_manager/logging/PebbleProbesCheck.java
@@ -0,0 +1,37 @@
+package edu.northwestern.cbits.purple_robot_manager.logging;
+
+import android.content.Context;
+
+import edu.northwestern.cbits.purple_robot_manager.R;
+import edu.northwestern.cbits.purple_robot_manager.probes.ProbeManager;
+import edu.northwestern.cbits.purple_robot_manager.probes.devices.PebbleProbe;
+import edu.northwestern.cbits.purple_robot_manager.probes.studies.LivewellPebbleActivityCountsProbe;
+import edu.northwestern.cbits.purple_robot_manager.triggers.DateTrigger;
+import edu.northwestern.cbits.purple_robot_manager.triggers.Trigger;
+import edu.northwestern.cbits.purple_robot_manager.triggers.TriggerManager;
+
+/**
+ * Created by Administrator on 3/29/15.
+ */
+public class PebbleProbesCheck extends SanityCheck
+{
+    public void runCheck(Context context)
+    {
+        PebbleProbe pebble = (PebbleProbe) ProbeManager.probeForName("edu.northwestern.cbits.purple_robot_manager.probes.devices.PebbleProbe", context);
+        LivewellPebbleActivityCountsProbe livewell = (LivewellPebbleActivityCountsProbe) ProbeManager.probeForName("edu.northwestern.cbits.purple_robot_manager.probes.studies.LivewellPebbleActivityCountsProbe", context);
+
+        this._errorMessage = null;
+        this._errorLevel = SanityCheck.OK;
+
+        if (pebble.isEnabled(context) && livewell.isEnabled(context))
+        {
+            this._errorLevel = SanityCheck.WARNING;
+            this._errorMessage = context.getString(R.string.name_sanity_pebble_probes_warning);
+        }
+    }
+
+    public String name(Context context)
+    {
+        return context.getString(R.string.name_sanity_pebble_probes_title);
+    }
+}
